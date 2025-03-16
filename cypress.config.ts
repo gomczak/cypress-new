f0e991938ce54fd6400a92d3c34f953b2c537738
slawekradzyminski <slawomir.radzyminski@gmail.com>
2025-03-16 12:44:10 +0100

    allure

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,10 +1,14 @@
 import { defineConfig } from "cypress";
+import { allureCypress } from "allure-cypress/reporter";
 
 export default defineConfig({
   e2e: {
     baseUrl: 'http://localhost:8081',
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      allureCypress(on, config, {
+        resultsDir: "allure-results",
+      });
+      return config;
     },
   },
 });
